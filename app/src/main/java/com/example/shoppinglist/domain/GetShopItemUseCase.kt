4ec33315e50348d9d3a9f2c4200172e190d134be
Artem <xamidoff.job@gmail.com>
2024-06-12 15:13:50 +0300

    add domain

---
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.shoppinglist.domain
+
+class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun getShopItem(id: Int): ShopItem {
+        return shopListRepository.getShopItem(id)
+    }
+
+}
